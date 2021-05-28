2ae4890219e350a79ff27b43050ecc4cbdc45fe1
rahmictas <rahmican_tas15@erdogan.edu.tr>
2021-05-29 02:02:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import requests
 import base64
 import urllib.request
-import urllib.error
 import urllib.parse
-import os
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+
+
 
 
 print('Dizinin bölüm linkini kopyala ve entere bas')
 site = input()
 r = requests.get(site)
 dosya = r.text
-baslangic = dosya.find('subtitle :')
-bitis = dosya.find('vpaidMode :')
-
-if bitis is None:
-    bitis = dosya.find('skipTime :')
+baslangic = dosya.find('vpaidMode :')
+bitis = dosya.find('skipTime :')
 
 
 
-a = baslangic+12
-b = bitis-105
+
+
+a = baslangic+59
+b = bitis-69
 dekod = ""
 
 for i in range(a, b+1):
     dekod += dosya[i]
-
 
 data = base64.b64decode(dekod)
 sonuc = data.decode("ascii")
@@ -35,6 +30,7 @@ uzanti = '.vtt'
 
 q = requests.get(sonuc)
 altyazi = q.text
+
 
 response = urllib.request.urlopen(sonuc)
 webcontent = response.read()
@@ -45,10 +41,3 @@ dosyaAdi = dosyaAd + uzanti
 f = open(dosyaAdi,'wb')
 f.write(webcontent)
 f.close()
-
-
-
-
-
-
-

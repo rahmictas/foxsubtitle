1b58df0f62d970b0dde7f4aab5815009235282e7
rahmictas <rahmican_tas15@erdogan.edu.tr>
2021-05-29 02:40:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,39 @@ import base64
 import urllib.request
 import urllib.parse
 
+while True:
+    print('Dizinin bölüm linkini kopyala ve entere bas ya da çıkmak için q ya bas')
+    site = input()
+    if site == "q":
+        break
+    elif site == "Q":
+        break
+    else:
+        r = requests.get(site)
+        dosya = r.text
+        baslangic = dosya.find('vpaidMode :')
+        bitis = dosya.find('skipTime :')
 
+        a = baslangic+59
+        b = bitis-69
+        dekod = ""
 
+        for i in range(a, b+1):
+            dekod += dosya[i]
 
-print('Dizinin bölüm linkini kopyala ve entere bas')
-site = input()
-r = requests.get(site)
-dosya = r.text
-baslangic = dosya.find('vpaidMode :')
-bitis = dosya.find('skipTime :')
+        data = base64.b64decode(dekod)
+        sonuc = data.decode("ascii")
+        uzanti = '.vtt'
 
+        q = requests.get(sonuc)
+        altyazi = q.text
 
+        response = urllib.request.urlopen(sonuc)
+        webcontent = response.read()
+        print('Dosya adı giriniz')
+        dosyaAd = input()
+        dosyaAdi = dosyaAd + uzanti
 
-
-
-a = baslangic+59
-b = bitis-69
-dekod = ""
-
-for i in range(a, b+1):
-    dekod += dosya[i]
-
-data = base64.b64decode(dekod)
-sonuc = data.decode("ascii")
-uzanti = '.vtt'
-
-q = requests.get(sonuc)
-altyazi = q.text
-
-
-response = urllib.request.urlopen(sonuc)
-webcontent = response.read()
-print('Dosya adı giriniz')
-dosyaAd = input()
-dosyaAdi = dosyaAd + uzanti
-
-f = open(dosyaAdi,'wb')
-f.write(webcontent)
-f.close()
+        f = open(dosyaAdi, 'wb')
+        f.write(webcontent)
+        f.close()
